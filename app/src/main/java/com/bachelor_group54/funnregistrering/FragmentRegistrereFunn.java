139598fe3e852b0458353cbe7fd15be46387dd16
Helge <helghelland@gmail.com>
2021-03-01 11:44:03 +0100

    Legger til kamera funksjon i nyttfunn fragmentet

--- app/src/main/java/com/bachelor_group54/funnregistrering/FragmentRegistrereFunn.java
+++ app/src/main/java/com/bachelor_group54/funnregistrering/FragmentRegistrereFunn.java
@@ -1,11 +1,16 @@
 package com.bachelor_group54.funnregistrering;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import androidx.annotation.NonNull;
@@ -28,17 +33,39 @@ public class FragmentRegistrereFunn extends Fragment {
         return view;
     }
 
-    public void bildeBtn(Context context) {
+    public void gpsBtn() {
         //TODO legge til bilde funksjon
-        Toast.makeText(context, "Har ikke laget bildefunksjon enda", Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(), "Har ikke laget gpsfunksjon enda", Toast.LENGTH_LONG).show();
     }
 
-    public void gpsBtn(Context context) {
-        //TODO legge til gps funksjon
-        Toast.makeText(context, "Har ikke laget gps funksjon enda", Toast.LENGTH_LONG).show();
+    int CAMERA_PIC_REQUEST = 1337; //Setting the request code for the camera intent, this is used to identify the result when it is returned after taking the picture in onActivityResult.
+
+    //This method opens the camera app when clicking the "Take image" button
+    public void bildeBtn(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //Makes an intent of the image capture type
+        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST); //Starts the camera app and waits for the result
     }
 
-    public void registrerFunnBtn(Context context) {
+    @Override
+    //This method receives the image from the camera app and setts the ImageView to that image.
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAMERA_PIC_REQUEST) { //If the requestCode matches that of the startActivityForResult of the cameraIntent we know it is the camera app that is returning it's data.
+            Bitmap picture; //Initializing the bitmap
+            try { //May produce null pointers if the picture is not taken
+                picture = (Bitmap) data.getExtras().get("data"); //Gets the picture from the camera app and saves it as a Bitmap
+            }catch (NullPointerException e){
+                Toast.makeText(getContext(), "Picture not taken", Toast.LENGTH_LONG).show(); //Prints a message to the user, explaining that no picture was taken
+                return; //Return if there is no picture
+            }
+
+            ImageView imageView = view.findViewById(R.id.preview_bilde_nytt_funn); //Finds the ImageView
+            imageView.setImageBitmap(picture); //Sets the ImageView to the picture taken from the camera app
+        }
+
+        super.onActivityResult(requestCode, resultCode, data); //Calls the super's onActivityResult (Required by Android)
+    }
+
+    public void registrerFunnBtn() {
         Funn funn = new Funn();
 
         EditText tittel = view.findViewById(R.id.nytt_funn_tittel_et); //Finner editText som holder tittelen
@@ -47,7 +74,7 @@ public class FragmentRegistrereFunn extends Fragment {
         EditText beskrivelse = view.findViewById(R.id.nytt_funn_beskrivelse_et); //Finner beskrivelse editText'en
         funn.setBeskrivelse(beskrivelse.getText().toString());//Legger beskrivelsen til i funnet
 
-        ObjektLagrer objektLagrer = new ObjektLagrer(context, "funn"); //Initialiserer objektLagret med context og filNavnet
+        ObjektLagrer objektLagrer = new ObjektLagrer(getContext(), "funn"); //Initialiserer objektLagret med context og filNavnet
         ArrayList<Object> arrayList = objektLagrer.loadData(); //Henter arrayet som alerede er lagret på filNavnet
         arrayList.add(funn); //Legger det nye funnet til i listen
 
