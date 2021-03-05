59815014ab2f7e3d0cee1f8540ca38886b40bdd8
Maplesyrup18 <aceasa08@hotmail.com>
2021-03-05 13:04:51 +0100

    Add project files.

---
+++ DAL/FunnDB.cs
@@ -0,0 +1,20 @@
+﻿using FunnregistreringsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnregistreringsAPI.DAL
+{
+    public class FunnDB : DbContext
+    {
+        public FunnDB(DbContextOptions<FunnDB> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Bruker> bruker { get; set; }
+        public DbSet<Funn> funn { get; set; }
+    }
+}
---
+++ Models/Bruker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunnregistreringsAPI.Models
+{
+    public class Bruker
+    {
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string Brukernavn { get; set; }
+        
+        public byte[] Passord { get; set; }
+        //Salt is used to hash passwords with unique functions
+        public byte[] Salt { get; set; }
+    }
+}
