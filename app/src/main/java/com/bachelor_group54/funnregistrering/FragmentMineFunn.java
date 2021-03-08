161b139fc708a8a843cf81902649e0de1b5a9121
Helge <helghelland@gmail.com>
2021-03-08 13:54:31 +0100

    Comments i FragmentMineFunn.java

--- app/src/main/java/com/bachelor_group54/funnregistrering/FragmentMineFunn.java
+++ app/src/main/java/com/bachelor_group54/funnregistrering/FragmentMineFunn.java
@@ -13,27 +13,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+//Class for the my finds fragment
 public class FragmentMineFunn extends Fragment {
     private View view;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_mine_funn, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_mine_funn, container, false); //Inflates the view
         makeList();
-        return view;
+        return view; //Returns the this fragment's view
     }
 
+    //This method creates and sets a list containing all the finds.
     public void makeList(){
-        ListView listView = view.findViewById(R.id.list_view_mine_funn);
+        ListView listView = view.findViewById(R.id.list_view_mine_funn); //Finds the listView
         ObjektLagrer objektLagrer = new ObjektLagrer(getContext(), "funn");
-        ArrayList funnListe = objektLagrer.loadData();
-        ListAdapter listAdapter = new ListAdapter(getContext(), funnListe);
-        listView.setAdapter(listAdapter);
+        ArrayList funnListe = objektLagrer.loadData(); //Gets the ArrayList containing all the finds
+        ListAdapter listAdapter = new ListAdapter(getContext(), funnListe); //Creates the listAdapter, the listAdapter is used to handle the elements within the listView
+        listView.setAdapter(listAdapter); //Sets the ListAdapter
     }
 }
