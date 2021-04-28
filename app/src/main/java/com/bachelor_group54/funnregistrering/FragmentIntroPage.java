0382de7b2dbcbc24c25c7f00de8b1b613dbe729d
T0rRA <48518584+T0rRA@users.noreply.github.com>
2021-04-28 17:05:36 +0200

    * Opprettet - FragmentIntroPage.java - fragment_intro_page.xml Opprettet introsiden som kommer opp første gang appen åpnes. Knapp for å hoppe videre til login.
    * Endret
    - MainActivity.java
    - fragment_main.xml
    Lagt til knapp for å hoppe til introsiden (slik at vi får den opp i appen under testing).

---
+++ app/src/main/java/com/bachelor_group54/funnregistrering/FragmentIntroPage.java
@@ -0,0 +1,27 @@
+package com.bachelor_group54.funnregistrering;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class FragmentIntroPage extends Fragment {
+    private View view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_intro_page, container, false); //Loads the page from the XML file
+        return view;
+    }
+}
