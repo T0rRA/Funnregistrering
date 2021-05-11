fe985e8e9ecf6b6ffae3852388c803f9dbe0225a
Helge <helghelland@gmail.com>
2021-05-11 11:29:43 +0200

    Laster in bruker info i editTextene

--- app/src/main/java/com/bachelor_group54/funnregistrering/FragmentBruker.java
+++ app/src/main/java/com/bachelor_group54/funnregistrering/FragmentBruker.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -22,6 +23,26 @@ public class FragmentBruker extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_bruker, container, false); //Loads the page from the XML file
+        updateEditTexts();
         return view;
+    }
+
+    public void updateEditTexts(){
+        User user = User.getInstance();
+
+        EditText userFirstNameEt = view.findViewById(R.id.fragment_bruker_fornavn);
+        userFirstNameEt.setText(user.getName());
+
+        EditText userLastNameEt = view.findViewById(R.id.fragment_bruker_etternavn);
+        userLastNameEt.setText(user.getLastName());
+
+        EditText userAddressEt = view.findViewById(R.id.fragment_bruker_adresse);
+        userAddressEt.setText(user.getAddress());
+
+        EditText userTlfEt = view.findViewById(R.id.fragment_bruker_tlf);
+        userTlfEt.setText(user.getPhoneNum());
+
+        EditText userEmailEt = view.findViewById(R.id.fragment_bruker_epost);
+        userEmailEt.setText(user.getEmail());
     }
 }
